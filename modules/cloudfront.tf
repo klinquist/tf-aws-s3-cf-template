dfa56ad92e1a5fb5d0afa820881d50d20c76f956
Kris Linquist <1343376+klinquist@users.noreply.github.com>
2023-10-05 08:44:02 -0700

    Increase minimum TLS version

--- modules/cloudfront.tf
+++ modules/cloudfront.tf
@@ -63,7 +63,7 @@ resource "aws_cloudfront_distribution" "my_cloudfront" {
     cloudfront_default_certificate = true
     acm_certificate_arn = aws_acm_certificate.certificate.arn
     ssl_support_method       = "sni-only"
-    minimum_protocol_version = "TLSv1"
+    minimum_protocol_version = "TLSv1.2_2021"
   }
 
 }
