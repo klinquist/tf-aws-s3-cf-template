9d567234ac91307ef0ca665e3946dd6d92c22b06
Kris Linquist <1343376+klinquist@users.noreply.github.com>
2024-01-03 09:54:00 -0800

    auto-creates a deploy and validate script

--- set-up-repo.sh
+++ set-up-repo.sh
@@ -126,30 +126,6 @@ fi
 read -r -p "Do you want to create a new repository from this folder, commit all files, and set up a github action to auto-deploy the '_site' subfolder to s3? (y/n): " answer
 case $answer in
     [Yy]* )
-        get_domain_name
-        # Remove existing git repo (that was cloned from the template)
-        rm -rf .git
-        git init
-        mkdir -p .github/workflows
-        mv sample-github-action/build-and-deploy.yml .github/workflows/deploy.yml
-        rm -rf sample-github-action
-        echo "Creating repo $domain_name"
-        gh repo create "$domain_name" --private --source=. --remote=origin
-        echo "Adding secrets to repo..."
-        gh secret set "AWS_S3_BUCKET_NAME" -b "$BUCKET" -a actions
-        gh secret set "AWS_CLOUDFRONT_DISTRIBUTION_ID" -b "$CLOUDFRONT_ID" -a actions
-        gh secret set "AWS_ACCESS_KEY_ID" -b "$ACCESS_KEY" -a actions
-        gh secret set "AWS_SECRET_ACCESS_KEY" -b "$SECRET" -a actions
-        echo "Committing files to repo..."
-        git add ./*
-        git add .github
-        git add .gitignore
-        git add .terraform.lock.hcl
-        git commit -m "Initial commit"
-        git branch -M main
-        git push -u origin main
-        echo "Done! In a few minutes, your site should be available at https://www.$domain_name"
-        exit 0
         ;;
     [Nn]* )
         showSecrets
@@ -160,3 +136,49 @@ case $answer in
         exit 1
         ;;
 esac
+
+get_domain_name
+
+# Name of the new script
+new_script_name="manually-deploy.sh"
+# Create and write to the new script
+cat << EOF > $new_script_name
+#!/bin/bash
+# Auto-created by https://www.github.com/klinquist/tf-aws-s3-cf-template
+
+# Sync _site directory with S3 bucket
+aws s3 sync _site/* s3://$domain_name --acl public-read --delete --cache-control max-age=604800
+
+# Invalidate CloudFront cache
+aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_ID --paths "/*"
+EOF
+
+# Make the new script executable
+chmod +x $new_script_name
+
+
+# Remove existing git repo (that was cloned from the template)
+rm -rf .git
+git init
+mkdir -p .github/workflows
+mv sample-github-action/build-and-deploy.yml .github/workflows/deploy.yml
+rm -rf sample-github-action
+echo "Creating repo $domain_name"
+gh repo create "$domain_name" --private --source=. --remote=origin
+echo "Adding secrets to repo..."
+gh secret set "AWS_S3_BUCKET_NAME" -b "$BUCKET" -a actions
+gh secret set "AWS_CLOUDFRONT_DISTRIBUTION_ID" -b "$CLOUDFRONT_ID" -a actions
+gh secret set "AWS_ACCESS_KEY_ID" -b "$ACCESS_KEY" -a actions
+gh secret set "AWS_SECRET_ACCESS_KEY" -b "$SECRET" -a actions
+echo "Committing files to repo..."
+git add ./*
+git add .github
+git add .gitignore
+git add .terraform.lock.hcl
+git commit -m "Initial commit"
+git branch -M main
+git push -u origin main
+echo ""
+echo "Script $new_script_name has been created and made executable.  You can use this if you ever want to manually deploy your code without using the github action."
+echo ""
+echo "Done! In a few minutes, your site should be available at https://www.$domain_name"
