357ecf8cfcd4b51fe66f1acb190e983e38b1ddac
Kris Linquist <kris@linquist.net>
2025-01-02 06:23:58 -0800

    Change google default MX records

--- modules/route53.tf
+++ modules/route53.tf
@@ -74,11 +74,7 @@ resource "aws_route53_record" "gmail" {
   type    = "MX"
   ttl     = 300
   records = [
-    "1 ASPMX.L.GOOGLE.COM",
-    "5 ALT1.ASPMX.L.GOOGLE.COM",
-    "5 ALT2.ASPMX.L.GOOGLE.COM",
-    "10 ASPMX2.GOOGLEMAIL.COM",
-    "10 ASPMX3.GOOGLEMAIL.COM",  
+    "1 SMTP.GOOGLE.COM",
   ]
 }
 
